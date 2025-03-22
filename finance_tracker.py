ab5b30139d878dcb19fd6dcc11ca1488867ef50e
Danny Tran <91554483+BrooklynD23@users.noreply.github.com>
2025-03-22 13:34:14 -0700

    Revert "Set-up Files & Outline Systems"
    This reverts commit 8524d0f1da1ed27ee9b7aac8b4d4cdf8f9c7c66c.

--- finance_tracker.py
+++
@@ -1,4 +0,0 @@
-# Finance Tracker Class 
-# We will use this for feature of: tracking & storing user's finance
-# Then create report (visualizing the user's finance)
-# SQLite for storing the data (?) - We <--- need to create security system
